dff38df15a08e4297c79c6c0c6f9afdfd619b77d
Ahsan-Wali758 <wali.8th@gmail.com>
2025-08-31 23:36:07 +0500

    Adding java script

---
+++ app.js
@@ -0,0 +1,358 @@
+// Question No 1
+/*var input = +prompt("Enter any number check its divisible by 3: ");
+
+if(input%3==0)
+{
+    console.log("The number is divisible by 3: ");
+}
+else{
+    console.log("The number is not divisible by 3: ");
+}*/
+
+// Question No 2
+
+/*var input =+prompt("Enter any number check is Even or Odd: ");
+
+if(input%2==0)
+{
+    console.log("The number is even: "+ input);
+}
+else if(input%2!=0)
+{
+    console.log("The number is Odd: "+input);
+}
+else{
+    console.log("Input is Invalid: ");
+}*/
+
+//Question No 3
+
+/*var input =+prompt("Enter any number chexk person is young or old: ");
+
+if(input >=18)
+{
+    console.log("The person is young: "+input);
+}
+else if(input>=50)
+{
+    console.log("The Person is old: ");
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+//Question No 4
+
+/*var input= prompt("Enter any name: ");
+
+if(input=="Ahsan")
+{
+    console.log("Your name is same as my name: " + input);
+}else{
+    console.log("Name is not same: " + input);
+}*/
+
+// Question No 5 same as Question No 1
+
+//Question No 6
+
+/*var input1 = +prompt("Enter any number: ");
+var input2 = prompt("Enter any character: ");
+
+if(input1%2==0 || input1%3==0)
+{
+    console.log("This is a Number: "+input1);
+}
+else if(input2==="a")
+{
+    console.log("This is a character: " +input2);
+}
+else{
+    console.log("Invalid input: ");
+}*/
+
+// Question No 7
+
+/*var num1=+prompt("Enter the value of num1 :");
+var op=prompt("Enter the operator: ");
+var num2=+prompt("Enter the value of num2: ");
+var res;
+
+if(op=="+")
+{
+    res=num1+num2;
+    console.log("Result: "+res);
+}
+else if(op=="-")
+{
+    res=num1-num2;
+    console.log("Resulr: "+res);
+}
+else if(op=="*")
+{
+    res=num1*num2;
+    console.log("result: "+res);
+}
+else if(op=="/")
+{
+    res=num1/num2;
+    console.log("Result: "+res);
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+// Question No 8
+
+/*var time1= +prompt("Enter the value of Time 1: ");
+var time2= +prompt("Enter the value of Time 2: ");
+
+if(time1>=0 && time2<=1200)
+{
+    console.log("Good Morning: "+ false);
+}
+else if(time1>=1200 && time2<=1700)
+{
+    console.log("Good Afternoon: "+ false);
+}
+else if(time1>=1700 && time2<=2100)
+{
+    console.log("Good Morning: "+true);
+}
+else if(time1>=2100 && time2<=2359)
+{
+    console.log("Good Night: " +false);
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+// Question No 9
+
+/*var input= +prompt("Enter a Year to check the year is laep Year or Not: ");
+
+if(input % 400==0)
+{
+    console.log("This a leap year: "+input);
+}
+else if(input % 100==0)
+{
+    console.log("This is not a leap year: " +input);
+}
+else if(input % 4==0)
+{
+    console.log("This is a leap year: " + input);
+}
+else{
+    console.log("Invalid Input: "+ input);
+}*/
+
+// Question No 10
+
+/*var input= prompt("Enter a password: ");
+
+if(input=="Ahsan")
+{
+    console.log("Your password is correct: "+input);
+}
+else{
+    console.log("Invalid Password: "+input);
+}*/
+
+//Qustion No 11
+
+/*var name=prompt("Enter your name: ");
+
+if(name=="Ahsan")
+{
+    document.write("Hello world: "+name);
+}*/
+
+//Question No 12
+
+/*var hour= +prompt("Enter the Hour: ");
+
+if(hour>=13)
+{
+    confirm.console.log("Good Evening: "+ hour);
+}
+else if(hour>=18)
+{
+    confirm.log("Good Evening: "+hour);
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+//Question No 13
+
+/*var num1= +prompt("Enter the value of num1: ");
+var num2= +prompt("Enter the value of num2: ");
+
+if(num1>num2)
+{
+    console.log("Number 1 is greater: "+num1);
+}
+else if(num1<num2)
+{
+    console.log("Number 2 is greater: "+num2);
+}
+else if(num1==num2)
+{
+    console.log("Number 1 and Number 2 is Equal: "+num1+" = "+num2);
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+//Question No 14
+
+/*var num= +prompt("Enter Any Number: ");
+
+if(num>0)
+{
+    console.log("The Number is Positive: "+num);
+}
+else if(num<0)
+{
+    console.log("The Number is Nagetive: "+num);
+}
+else if(num==0)
+{
+    console.log("The Number is Zero: "+num);
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+//Question No 15
+
+/*var hour1= +prompt("Enter the value of hour1: ");
+var hour2= +prompt("Enter the value of hour2: ");
+
+if(hour1==6 || hour2==9)
+{
+    console.log("Breakfast is served: ");
+}
+else if(hour1==11 || hour2==13)
+{
+    console.log("Time for lunch: ");
+}
+else if(hour1==17 || hour2==21)
+{
+    console.log("Its Dinner Time: ");
+}
+else{
+    console.log("Sorry You,ll have to wait: ");
+}*/
+
+// Question No 16
+
+/*var type1= prompt("Enter Any number: ");
+
+
+if(type1%2==0 || type1%3==0)
+{
+    console.log("This is a Number: ");
+}
+else if(type1=="Ahsan")
+{
+    console.log("This is a String: ");
+}
+else if(type1=true)
+{
+    console.log("This is a Boolean type: ");
+}*/
+
+/*var word= prompt("Wnter any vovel: ");
+
+if(word=="a" || word=="e" || word=="i" ||word=="o" || word=="u")
+{
+    console.log("This is a vovel: ");
+}
+else{
+    console.log("This is not a vovel: ");
+}*/
+
+//Question No 18
+
+/*var num1= +prompt("Enter any number: ");
+var num2= +prompt("Enter nuy number: ");
+
+if(num1>num2)
+{
+    console.log("Number1 is Grester: ");
+}
+else if(num1<num2)
+{
+    console.log("Number2 is Greater: ");
+}
+else{
+    console.log("Invalid Input: ");
+}*/
+
+//Question No 
+
+/*var month= +prompt("Enter any Number For month: ");
+
+if(month==1)
+{
+    alert("Januaray");
+}
+if(month==2)
+{
+    alert("Februray");
+}
+if(month==3)
+{
+    alert("March");
+}
+if(month==4)
+{
+    alert("April");
+}
+if(month==5)
+{
+    alert("May");
+}
+if(month==6)
+{
+    alert("June");
+}
+if(month==7)
+{
+    alert("July");
+}
+if(month==8)
+{
+    alert("August");
+}
+if(month==9)
+{
+    alert("September");
+}
+if(month==10)
+{
+    alert("October");
+}
+if(month==11)
+{
+    alert("November");
+}
+if(month==12)
+{
+    alert("December");
+}*/
+
+//Question No 20
+
+var age= +prompt("Enter the person age: ");
+
+if(age>=18 && age<=40)
+{
+    console.log("The person is Young: ");
+}
+else if(age>=50 && age<=100)
+{
+   console.log("The person is old: ");
+}
